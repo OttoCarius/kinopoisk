991a7f17f14be53553c1c19285bbe1c8743cf37f
OttoCarius <ottocarius1993@gmail.com>
2022-09-24 10:04:22 +0300

    deleted all work and started again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,29 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { lazy } from 'react';
+// import { Routes, Route } from 'react-router-dom';
 
-import SharedLayout from './SharedLayout/SharedLayout';
+// import SharedLayout from './SharedLayout/SharedLayout';
 
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+// const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 
-const MoviePage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+// const MoviePage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
 
-const MovieDetailsPage = lazy(() =>
-  import('../pages/MovieDetailsPage/MovieDetailsPage')
-);
+// const MovieDetailsPage = lazy(() =>
+//   import('../pages/MovieDetailsPage/MovieDetailsPage')
+// );
 
-const PageNotFound = lazy(() => import('../pages/PageNotFound/PageNotFound'));
+// const PageNotFound = lazy(() => import('../pages/PageNotFound/PageNotFound'));
 
-export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviePage />} />
-          <Route path="movies/:movieId" element={<MovieDetailsPage />}></Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
-};
+// export const App = () => {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+//           <Route index element={<HomePage />} />
+//           <Route path="movies" element={<MoviePage />} />
+//           <Route path="movies/:movieId" element={<MovieDetailsPage />}></Route>
+//           <Route path="*" element={<PageNotFound />} />
+//         </Route>
+//       </Routes>
+//     </>
+//   );
+// };
